8a73a6258640bdc443afcda5c8807e411a4b117a
HedvigM <hedvig@mejstedt.se>
2021-04-19 20:58:21 +0200

    trying the git-connection

---
+++ mixed-messages.js
@@ -0,0 +1,47 @@
+Vad säger en Göteborgare till ett Star Wars fan?
+– JeeDaaaj.
+
+Vad sa den ena kannibalen till den andra?
+– E du go eller?
+
+Varför är det ingen snö på gatorna i Glasgow på vintern?
+– De e la klart. 600 000 skottar.
+
+Gick det bra med rymdresan? 
+– Nej, den blev uppskjuten.
+
+Hur vet man att en dykarkurs är inställd?
+- Instruktören dyker inte upp!
+
+Hur tröstar man en vegan?
+– Soja, soja!
+
+Hur många bor det i Stockholm?
+– En dryg miljon!
+
+Vilket djur är jordens största?
+– Eeeen-orm!
+
+En göteborgare frågade Jesus, vad gör du i påsk?
+– Vet inte, jag har inget spikat.
+
+Hur många göteborgare bor det i Kanada?
+– Åttava?
+
+Hur tar man med sig en hund genom tullen?
+– Tax-Free såklart!
+
+Vilken är världens mest musikaliska fågel? 
+– Truten, för det är en måsart. 
+
+Vad anser du om läget i Angola?
+– De Ango-la-inte mig!
+
+I vilken stad tar bilköerna aldrig slut?
+– Konstantinopel.
+
+Vad är tandläkarnas favoritsport?
+- Fluorball.
+
+Ser du mycket på Ishockey?
+– Nej, bara periodvis. 
