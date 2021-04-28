5f6406a3617b5e36680d8c559ce753c72617b852
HedvigM <hedvig@mejstedt.se>
2021-04-28 20:41:15 +0200

    array jokes

--- messages.js
+++ messages.js
@@ -1,65 +1,35 @@
 function getMessage() {
-    randomNumber = Math.floor(Math.random() * 3);
-    switch (randomNumber) {
-        case 0:
-            return 'Vad säger en Göteborgare till ett Star Wars fan? - JeeDaaaj.';
-        case 1:
-            return 'Vad sa den ena kannibalen till den andra? - E du go eller?';
-        case 2:
-            return 'Varför är det ingen snö på gatorna i Glasgow på vintern? - De e la klart. 600 000 skottar.';
-    }
+    randomNumber = Math.floor(Math.random() * 14);
+    const message = ["Vad säger en Göteborgare till ett Star Wars fan? - JeeDaaaj", 
+    "Varför är det ingen snö på gatorna i Glasgow på vintern? - De e la klart. 600 000 skottar.",
+    "Vad sa den ena kannibalen till den andra? - E du go eller?",
+    "Gick det bra med rymdresan? – Nej, den blev uppskjuten.",
+    "Hur vet man att en dykarkurs är inställd? - Instruktören dyker inte upp!",
+    "Hur många bor det i Stockholm? – En dryg miljon!", 
+    "Vilket djur är jordens största? – Eeeen-orm!",
+    "En göteborgare frågade Jesus, vad gör du i påsk? – Vet inte, jag har inget spikat.", 
+    "Hur många göteborgare bor det i Kanada? – Åttava?", 
+    "Hur tar man med sig en hund genom tullen? – Tax-Free såklart!",
+    "Vilken är världens mest musikaliska fågel? – Truten, för det är en måsart.",
+    "Vad anser du om läget i Angola? – De Ango-la-inte mig!",
+    "I vilken stad tar bilköerna aldrig slut? – Konstantinopel.",
+    "Vad är tandläkarnas favoritsport? - Fluorball.", 
+    "Ser du mycket på Ishockey? – Nej, bara periodvis."] 
+    return message[randomNumber];
 }
-console.log(getMessage());
-
-
-
-const knapp = document.createElement("getMessage");
 
 
 
 
+const knapp = document.getElementById("getMessage");
+const jokeDiv = document.getElementById('joke')
 
-knapp.addEventListener ("click", function() {
-  alert("did something");
+knapp.addEventListener("click", function () {
+    console.log(jokeDiv)
+    const joke = getMessage();
+    console.log(joke);
+    jokeDiv.innerHTML = joke
 });
 
 
-/*
-Gick det bra med rymdresan? 
-– Nej, den blev uppskjuten.
 
-Hur vet man att en dykarkurs är inställd?
-- Instruktören dyker inte upp!
-
-Hur tröstar man en vegan?
-– Soja, soja!
-
-Hur många bor det i Stockholm?
-– En dryg miljon!
-
-Vilket djur är jordens största?
-– Eeeen-orm!
-
-En göteborgare frågade Jesus, vad gör du i påsk?
-– Vet inte, jag har inget spikat.
-
-Hur många göteborgare bor det i Kanada?
-– Åttava?
-
-Hur tar man med sig en hund genom tullen?
-– Tax-Free såklart!
-
-Vilken är världens mest musikaliska fågel? 
-– Truten, för det är en måsart. 
-
-Vad anser du om läget i Angola?
-– De Ango-la-inte mig!
-
-I vilken stad tar bilköerna aldrig slut?
-– Konstantinopel.
-
-Vad är tandläkarnas favoritsport?
-- Fluorball.
-
-Ser du mycket på Ishockey?
-– Nej, bara periodvis. */
